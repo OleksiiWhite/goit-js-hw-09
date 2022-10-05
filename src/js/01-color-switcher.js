2549fb5b149248a14986e1724f8700ee854a68bc
Oleksii Biletskyi <alenikbel@ukr.net>
2022-10-05 15:37:23 +0300

    modul 9 task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('.js-start');
+const stopBtn = document.querySelector('.js-stop');
+const firstMenuItem = document.querySelector('.menu-item');
 
+firstMenuItem.style.textAlign = `center`;
+
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    firstMenuItem.style.backgroundColor = `#${Math.floor(
+      Math.random() * 16777215
+    ).toString(16)}`;
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
